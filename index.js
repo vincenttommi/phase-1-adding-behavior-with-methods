fbc59a934c6cceaa67280a1f4821b14d0023b709
vincenttommi <vincenttommi@gmail.com>
2023-03-31 18:02:33 +0300

    constructor

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+
+
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+
+
+
+class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+
+
+  class Bird {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `It's me! ${this.name}, the parrot!`;
+    }
+  }
+
+  
+  
+  class Bird2 {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says squawk!`;
+    }
+  }
